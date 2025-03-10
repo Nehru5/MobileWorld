a741975a02755f2bd9daf4fdac3772b89400f419
Nehru raja <141004417+Nehru5@users.noreply.github.com>
2025-03-10 08:05:41 +0530

    Delete shopping directory

--- shopping/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from shopping.models import UserProfile, UserDetails, Product, Order, Cart
-
-admin.site.register(UserProfile)
-admin.site.register(UserDetails)
-admin.site.register(Product)
-admin.site.register(Order)
-admin.site.register(Cart)
-
