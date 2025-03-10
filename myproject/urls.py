1a9670daa6a7ba92ed5ac4bfeb05382c1a049d61
Nehru raja <141004417+Nehru5@users.noreply.github.com>
2025-03-10 11:14:21 +0530

    Update urls.py

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from django.conf import settings
 from django.conf.urls.static import static
-from shopping.views import home, signup_page, login_page,display, logout_page, products, product_details,add_details, Add_To_Cart, Cart_data
+from django.views.static import serve
+from shopping.views import home, signup_page, login_page, display, logout_page, products, product_details, add_details, Add_To_Cart, Cart_data
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -16,13 +17,10 @@ urlpatterns = [
     path("add/", add_details, name="add_details"),
     path("cart/<int:id>/", Add_To_Cart, name="cart"),
     path("cart/", Cart_data, name="cartdata"),
-  
-   
-    
-    
-   
-    
 ]
 
-if settings.DEBUG:
-    urlpatterns  = urlpatterns + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+# ✅ Serve media files manually when DEBUG=False
+if not settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
